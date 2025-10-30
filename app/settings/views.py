7cb196b883210b05ca3440c05228a79b04c60f22
beksultan <nurlanuuulubeksultan@gmail.com>
2025-10-30 18:50:38 +0600

    lesson 6

--- app/settings/views.py
+++ app/settings/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView,\
 RetrieveAPIView, DestroyAPIView
 from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins
+from rest_framework import mixins, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from app.settings.models import Category, ModelProduct, Product
 from app.settings.serilizers import CategorySerializer, ModelProductSerializer,\
 ProductSerializer
+from app.settings.pagination import StandartPagination
+from app.settings.filters import ProductFilter
 
 class CategoryAPIView(ListAPIView):
     queryset = Category.objects.all()
@@ -35,6 +38,7 @@ class ModelProductAPI(GenericViewSet,
                     mixins.DestroyModelMixin):
     queryset = ModelProduct.objects.all()
     serializer_class = ModelProductSerializer
+    pagination_class = StandartPagination
 
 class ProductAPI(GenericViewSet,
                 mixins.ListModelMixin,
@@ -44,3 +48,9 @@ class ProductAPI(GenericViewSet,
                 mixins.DestroyModelMixin):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = StandartPagination
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = ProductFilter
+    search_fields = ['name', 'description', "category__name", "model__name", "user"]
+    ordering_fields = ['price', 'created_at']
--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
     # rest
     'rest_framework',
+    'django_filters',
 
     'app.settings',
 ]
@@ -121,3 +122,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',  
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
